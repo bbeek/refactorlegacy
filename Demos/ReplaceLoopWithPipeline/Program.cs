27c67fa4ef34939e6c7f9b904993f7db1972b219
Bas van Beek <74142365+bbeek@users.noreply.github.com>
2021-01-10 18:40:16 +0100

    feat: Demo setup for Replace Loop With Pipeline

---
+++ Demos/ReplaceLoopWithPipeline/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace ReplaceLoopWithPipeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string fileName = "OfficeData.csv";
+
+            using var filestream = new StreamReader(fileName);
+            var norway = new OfficeReader().GetNorwayOffices(filestream.ReadToEnd());
+
+            foreach(var office in norway)
+            {
+                Console.WriteLine($"Hello from {office.City} ");
+            }
+        }
+    }
+}
