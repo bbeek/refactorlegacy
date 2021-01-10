60fcba87bc25a3973a9fb256644dc6b7742b4750
Bas van Beek <74142365+bbeek@users.noreply.github.com>
2021-01-10 18:40:13 +0100

    test: Add invalid case

--- Demos/Tests/SplitPhaseTests/PriceCalculatorTests.cs
+++ Demos/Tests/SplitPhaseTests/PriceCalculatorTests.cs
@@ -1,6 +1,8 @@
 using FluentAssertions;
 using NUnit.Framework;
 using SplitPhase;
+using System;
+using System.Collections.Generic;
 
 namespace SplitPhaseTests
 {
@@ -29,7 +31,39 @@ namespace SplitPhaseTests
 
             var outcome = sut.Calculate(order);
             outcome.Should().Be(expectedPrice);
+        }
 
+        [Test]
+        public void CalculatePrice_order_containing_unknown_product_should_throw_keynotfound()
+        {
+            const string order = "-Fanta 3";
+            
+            var sut = new PriceCalculator();
+
+            Action act = () => sut.Calculate(order);
+            act.Should().Throw<KeyNotFoundException>();
+        }
+
+        [Test]
+        public void CalculatePrice_order_without_spaces_should_throw_keynotfound()
+        {
+            const string order = "-Beer3";
+
+            var sut = new PriceCalculator();
+
+            Action act = () => sut.Calculate(order);
+            act.Should().Throw<KeyNotFoundException>();
+        }
+
+        [Test]
+        public void CalculatePrice_order_without_markup_should_throw_indexoutofrange()
+        {
+            const string order = "Beer 3";
+
+            var sut = new PriceCalculator();
+
+            Action act = () => sut.Calculate(order);
+            act.Should().Throw<IndexOutOfRangeException>();
         }
     }
 }
