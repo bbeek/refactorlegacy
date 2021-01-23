effe78c6728ef892e9887274a27b8fefad3c4cb1
Bas van Beek <74142365+bbeek@users.noreply.github.com>
2021-01-23 22:26:51 +0100

    Added sprout class demo

---
+++ Demos/Tests/SproutClassTests/QuarterlyReporTableHeaderProducerTest.cs
@@ -0,0 +1,20 @@
+using FluentAssertions;
+using SproutClass;
+using Xunit;
+
+namespace SproutClassTests
+{
+    public class QuarterlyReporTableHeaderProducerTest
+    {
+        //[Fact]
+        //public void MakeHeader_should_return_header()
+        //{
+        //    const string expectedHeader = "<tr><td>Department</td><td>Manager</td><td>Profit</td><td>Expenses</td></tr>";
+        //    var producer = new QuarterlyReporTableHeaderProducer();
+
+        //    var header = producer.MakeHeader();
+
+        //    header.Should().Be(expectedHeader);
+        //}
+    }
+}
