d6c98c816fd98c19a3a5ed7255bc81df35b13938
Bas van Beek <74142365+bbeek@users.noreply.github.com>
2021-01-30 14:48:16 +0100

    Suggestions for characterization tests

--- Labs/Day2/test/Day2Tests/PricesTest.cs
+++ Labs/Day2/test/Day2Tests/PricesTest.cs
@@ -2,6 +2,7 @@ using Day2.Controllers;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Day2Tests
@@ -9,9 +10,82 @@ namespace Day2Tests
     public class PricesTest
     {
         [Fact]
-        public async System.Threading.Tasks.Task Test1Async()
+        public async Task GetAsync_price_for_child_nightticket()
         {
-            
+            // Arrange
+
+            // Act
+
+            // Assert
+            // TODO: Verify actual behaviour
+        }
+
+        [Fact]
+        public async Task GetAsync_price_for_adult_nightticket()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            // TODO: Verify actual behaviour
+        }
+
+        [Fact]
+        public async Task GetAsync_price_for_senior_nightticket()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            // TODO: Verify actual behaviour
+        }
+
+        [Fact]
+        public async Task GetAsync_price_for_child_dayticket()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            // TODO: Verify actual behaviour
+        }
+
+        [Fact]
+        public async Task GetAsync_price_for_senior_dayticket()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            // TODO: Verify actual behaviour
+
+        }
+
+        [Fact]
+        public async Task GetAsync_price_for_below15_dayticket()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            // TODO: Verify actual behaviour
+        }
+
+        [Fact]
+        public async Task GetAsync_price_for_adult_dayticket_for_a_monday_not_a_holiday()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            // TODO: Verify actual behaviour
+
         }
     }
 }
